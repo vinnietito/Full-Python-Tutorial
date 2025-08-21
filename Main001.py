9dbb60fbbe748d6117083363b9f36e018d05311b
vinnietito <vincentnjuguna69@gmail.com>
2025-08-21 17:06:23 +0300

    Commits

---
+++ Main001.py
@@ -0,0 +1,20 @@
+
+#This is my first Python in this Tutorial
+print("We remain hopeful that one day it shall be well")
+print("InshaAllah")
+
+#Variables - container for a value(String, Integer, Float, Bollean)
+#Strings
+first_name = "Vincent"
+food = "Chapo"
+email = "vincentnjuguna69@gmail.com"
+
+#Integers
+age = 23
+quantity = 5
+
+
+
+#Output
+print(f"Hello {first_name}, You are {age} years old and you like {food} aloot")
+print(f"You are buying {quantity} items")
