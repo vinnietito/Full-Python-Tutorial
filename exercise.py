123840e1f8dfdbdda78d9291f20e4c8ec97cba61
vinnietito <vincentnjuguna69@gmail.com>
2025-08-21 18:48:40 +0300

    Commits

--- exercise.py
+++ exercise.py
@@ -7,12 +7,30 @@
 
 #print(f"The area of the Rectangle: {length}cm X {width}cm = {rectangle_area}cm²")
 
+
+
+
 # Exercise 2 Shopping Cart Program
 
-item = input("What item would like to buy?: ")
-price = float(input("What is the price?: "))
-quantity = int(input("How many would you like?: "))
-total = price * quantity
+# item = input("What item would like to buy?: ")
+# price = float(input("What is the price?: "))
+# quantity = int(input("How many would you like?: "))
+# total = price * quantity
 
-print(f"You have bought {quantity} X {item}/s")
-print(f"Your total is: ksh.{total}")
+# print(f"You have bought {quantity} X {item}/s")
+# print(f"Your total is: ksh.{total}")
+
+
+#A game of Madlibs
+
+
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing)")
+adjective2 = input("Enter an adjective (description)")
+verb1 = input("Enter a verb ending with 'ing'")
+adjective3 = input("Enter an adjective (description)")
+
+print(f"Today i went to to a {adjective1} zoo.")
+print(f"In an exhibit, i saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
