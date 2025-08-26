3a34b26875641ec7f637e7df5ffa7bdae7fdec63
vinnietito <vincentnjuguna69@gmail.com>
2025-08-26 22:21:39 +0300

    Commits

--- builtInMathFunction.py
+++ builtInMathFunction.py
@@ -1,11 +1,23 @@
-x = 3.14
-y = 4
-z = 5
+#x = 3.14
+#y = 4
+#z = 5
 
 #result = round(x)
 #result = abs(y)
 #result = pow(4, 3)
 #result = max(x, y, z)
-result = min(x, y, z)
+#result = min(x, y, z)
+
+#print(result)
+
+import math
+
+x = 9.1
+
+#print(math.pi)
+#print(math.e)
+#result = math.sqrt(x)
+#result = math.ceil(x)
+result = math.floor(x)
 
 print(result)
