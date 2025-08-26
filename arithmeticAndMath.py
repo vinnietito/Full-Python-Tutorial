8f0573be89f71c955ea8a56f3e8493d7595f39c5
vinnietito <vincentnjuguna69@gmail.com>
2025-08-26 22:03:27 +0300

    Commits

--- arithmeticAndMath.py
+++ arithmeticAndMath.py
@@ -6,7 +6,13 @@ friends = 5
 # friends += friends
 # friends = friends -2
 # friends -= friends
-friends = friends * 3
+# 
+# friends = friends * 3
+#friends *=3
+#friends = friends / 2
+#friends /=2
+#friends = friends ** 2
+#friends **= 2
+remainder = friends % 2
 
-
-print(friends)
+print(remainder)
